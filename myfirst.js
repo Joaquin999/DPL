c3abcbf50632129776fad692a63322f0abbd6947
Joaquin999 <32464518+Joaquin999@users.noreply.github.com>
2019-01-31 19:25:29 +0000

    Update myfirst.js

--- myfirst.js
+++ myfirst.js
@@ -2,6 +2,6 @@ var http=require('http');
 
 http.createServer(function(req,res){
       res.writeHead(200,{'Content-Type':'text/html'});
-      res.end('<head><meta charset="UTF-8"></head>Joaquín');
+      res.end('<head><meta charset="UTF-8"></head>Automaticos');
       console.info("Corriendo en el puerto 8080");
 }).listen(process.env.PORT);
