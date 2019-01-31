4d520477b411993fe0170572ab553a2f0116983d
Joaquin999 <32464518+Joaquin999@users.noreply.github.com>
2019-01-31 18:46:39 +0000

    Update myfirst.js

--- myfirst.js
+++ myfirst.js
@@ -4,4 +4,4 @@ http.createServer(function(req,res){
       res.writeHead(200,{'Content-Type':'text/html'});
       res.end('<head><meta charset="UTF-8"></head>Joaquín de León del Toro');
       console.info("Corriendo en el puerto 8080");
-}).listen(8080);
+}).listen(process.env.PORT);
